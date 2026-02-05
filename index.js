dc57a6f4d9621f290c32a6ff622e87619f9e0cec
Raj Kuwar <therajkuwar@gmail.com>
2026-02-05 14:37:52 +0530

    feat: add request logger middleware

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ import express from 'express'
 const app = express();
 const PORT = 8080;
 
+app.use((req, res, next)=>{
+    const time = new Date().toLocaleTimeString();
+    console.log(`[${time}] ${req.method} request to ${req.url}`);
+    next();
+})
+
 app.get('/', (req, res)=> {
     res.send('home');
 })
