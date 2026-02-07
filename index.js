89790170611a327147c0e47261aa3ec0c856ad57
Raj Kuwar <therajkuwar@gmail.com>
2026-02-07 14:51:47 +0530

    feat/added error handling middleware

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ app.use(morgan('dev')); // Automatically logs: GET /api/users 200 5ms
 app.use(loggingMiddleware);
 app.use('/api/users', userRouter);
 
+app.use((err, req, res, next)=> {
+    console.error(err.stack);
+
+    const status = err.status || 500;
+    const message = err.message || "Internal Server Error";
+
+    res.status(status).json({
+        error: message,
+        status: status
+    });
+})
+
+
 app.listen(PORT, ()=>{
     console.log(`server listening at port ${PORT}`);
 });
